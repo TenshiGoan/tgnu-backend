761a62e13ad356968c47561ff61782c16606df05
TenshiGoan <angelgoan@gmail.com>
2021-11-25 13:15:54 +0300

    0.0.2

--- src/module.ts
+++ src/module.ts
@@ -1,7 +1,33 @@
 import { defineNuxtModule } from "@nuxt/kit-edge";
+import Globby from "globby";
+import Consola from "consola";
+import Path from "path";
+
+const logger = Consola.withScope("backend");
 
 export default defineNuxtModule({
   name: "@tgnu/backend",
   configKey: "backend",
-  setup() {},
+  defaults: {
+    dir: "backend",
+  },
+  async setup(options, nuxt) {
+    const func_dir = Path.resolve(
+      nuxt.options.srcDir,
+      options.dir,
+      "functions"
+    );
+    const files_promise = useDir(func_dir);
+
+    nuxt.hook("modules:done", async () => {
+      const files = await files_promise;
+      logger.info(files);
+    });
+  },
 });
+
+async function useDir(dir: string) {
+  const pattern = "**/*.{ts,mjs,js,cjs}";
+  const files = await Globby(pattern, { cwd: dir });
+  return files;
+}
